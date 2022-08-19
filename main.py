379faeb95cc3f7a889b048d3981b61d4948641e1
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-19 17:35:55 +0300

    Add function predict_rub_salary

--- main.py
+++ main.py
@@ -19,9 +19,29 @@ def print_salary(programing_language):
         print(vacancy["salary"])
 
 
+def predict_rub_salary(vacancy):
+    salary = vacancy["salary"]
+    if salary:
+        if salary["currency"] == "RUR":
+            if salary['from'] and salary['to']:
+                return (salary['from'] + salary['to']) / 2
+            elif salary['from']:
+                return salary['from'] * 1.2
+            elif salary['to']:
+                return salary['to'] * 0.8
+
+
 def get_count_vacancies(programing_language):
     count = get_response(programing_language)["found"]
     return count
+
+
+def statistic_count_vacancies(programing_languages):
+    languages_statistic = {}
+    for lang in programing_languages:
+        count = get_count_vacancies(lang)
+        languages_statistic[lang] = count
+    print(languages_statistic)
 
 
 def main():
@@ -37,12 +57,11 @@ def main():
         "C",
         "TypeScript",
     ]
-    languages_statistic = {}
-    for lang in programing_languages:
-        count = get_count_vacancies(lang)
-        languages_statistic[lang] = count
-    print(languages_statistic)
-    print_salary("Python")
+    decoded_response = get_response("Python")
+    vacancies = decoded_response["items"]
+    for vacancy in vacancies:
+        salary = predict_rub_salary(vacancy)
+        print(salary)
 
 
 if __name__ == "__main__":
