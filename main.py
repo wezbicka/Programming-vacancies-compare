d45c612c2fce3207a6c2d3e124af6624bed414a6
Polina <75391420+wezbicka@users.noreply.github.com>
2022-08-19 16:03:28 +0300

    Add function get_count_vacancies for languages

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import requests
 
-url = "https://api.hh.ru/vacancies/"
-payload = {
-    "text": "Программист",
-    "area": "1",
-    "period": 30
-    }
-response = requests.get(url, params=payload)
-response.raise_for_status()
-print(response.json())
-print(response.json()["found"])
-print(response.url)
+
+def get_count_vacancies(programing_language):
+    url = "https://api.hh.ru/vacancies/"
+    payload = {
+        "text": f"Программист {programing_language}",
+        "area": "1",
+        "period": 30
+        }
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()["found"]
+
+
+def main():
+    programing_languages = [
+        "GO",
+        "JavaScript",
+        "Java",
+        "Python",
+        "Ruby",
+        "PHP",
+        "C++",
+        "C#",
+        "C",
+        "TypeScript",
+    ]
+    languages_statistic = {}
+    for lang in programing_languages:
+        count = get_count_vacancies(lang)
+        languages_statistic[lang] = count
+    print(languages_statistic)
+
+
+if __name__ == "__main__":
+    main()
